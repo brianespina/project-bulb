27d17dd6e09f8aa45a12ae6cf40baa6ecbcf9ec7
espinabrian <espinabrian@gmail.com>
2023-04-28 19:40:40 +0800

    created content boxes section

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,6 +14,11 @@ const firebaseConfig = {
     appId: "1:536751565270:web:87449839953db951849c77"
 };
 
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };

15308f35d0fbb4604728eec77532689e2d01aa28
espinabrian <espinabrian@gmail.com>
2023-05-04 16:39:07 +0800

    fixed type errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,49 @@
-
 import { Inter } from 'next/font/google'
-import { GetStaticProps, InferGetStaticPropsType, GetStaticPropsResult } from 'next'
-import { PrismaClient } from '@prisma/client'
+import { InferGetStaticPropsType } from 'next'
+import { PrismaClient, User } from '@prisma/client'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
-interface Boxes {
-  title: string
-  content: string
-}
-
-interface ContentBoxes {
-  layout: string
-  itemsPerRow: number
-  heading: string
-  content: Boxes[]
-  width: string
-  alignment: string
-  theme: string
-}
-
 
 export default function Home({ user }: InferGetStaticPropsType<typeof getStaticProps>) {
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-
+      <p>{user.fname} {user.lname}</p>
     </main>
   )
 }
 
-export const getStaticProps: GetStaticProps<{ section: ContentBoxes }> = async () => {
+export const getStaticProps = async () => {
 
   const prisma = new PrismaClient();
+  await prisma.product.deleteMany();
   await prisma.user.deleteMany();
-  await prisma.user.create({
+  let user = await prisma.user.create({
     data: {
       email: "espinabrian@gmail.com",
       fname: "Brian Nicolas",
-      lname: "Espina"
+      lname: "Espina",
+      sold: {
+        create: {
+          name: "Tatami Gi",
+          desc: "good tatami Gi",
+          price: 2,
+          condition: "second hand"
+        }
+      }
+    },
+    include: {
+      sold: true
     }
   })
 
   return {
     props: {
-      user: {}
+      user: JSON.parse(JSON.stringify(user)) as User
     }
   }
-
 }
-
